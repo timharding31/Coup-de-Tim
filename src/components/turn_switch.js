fe99596981ec3a87682c86497514d19e82d5a742
timharding <timharding31@gmail.com>
2020-09-28 18:18:56 -0700

    Update color of game over modal & debug turn loop at game end

--- src/components/turn_switch.js
+++ src/components/turn_switch.js
@@ -1,6 +1,7 @@
 import Turn from './turn';
 
 export default (game, action, turnCallBack) =>  {
+  if (game.gameOver) return
   switch(action) {
     case 'Income':
       return new Turn(game, turnCallBack, action, {
@@ -69,46 +70,5 @@ export default (game, action, turnCallBack) =>  {
       return;
   }
 }
-
-// Income:
-// just go through
-
-// Foreign Aid:
-// offer target chance to block
-// if !blocked, allow action to go through
-  // if blocked, offer player chance to challenge
-  // if blocked & challenged, make target prove Duke
-    // if blocked & !challenged, don't allow action to go through
-    // if blocked & challenged & proved, don't allow action to go through, return target's duke & give a new card
-    // if blocked & challenged & !proved, allow action to go through, present forfeitChoice to target
-
-// Tax:
-// offer target chance to challenge
-// if !challenged, allow action to go through
-// if challenged, make player prove Duke
-  // if challenged & proved, ALLOW action to go through, return player's duke & give new card
-  // if challenged & !proved, DON'T ALLOW action to go through, present forfeitChoice to player
-
-// Steal:
-// offer target chance to block OR challenge
-// if !blocked & !targetChallenged, ALLOW action to go through
-// if !blocked & targetChallenged, make player prove Captain
-  // if !blocked & targetChallenged & playerProved, ALLOW action to go through, return player's Captain & give new card
-  // if !blocked & targetChallenged & !playerProved, DONT ALLOW & present forfeitChoice to player
-// if blocked & !targetChallenged, offer player chance to challenge
-  // if blocked & playerChallenged, make target prove Captain or Ambassador
-    // if blocked & playerChallenged & targetProved, DONT ALLOW, return target's Capt/Amb & give a new card
-    // if blocked & playerChallenged, & !targetProved, ALLOW & present forfeitChoice to target
-
-// Assassinate:
-// offer target chance to block OR challenge
-// if !blocked & !targetChallenged, present forfeitChoice to target
-// if !blocked & targetChallenged, make player prove Assassin
-  // if !blocked & targetChallenged & playerProved, player WINS
-  // if !blocked & targetChallenged & !playerProved, DONT ALLOW, present forfeitChoice to player
-// if blocked & !targetChallenged, offer player chance to challenge
-  // if blocked & playerChallenged, make target prove Contessa
-    // if blocked & playerChallenged & targetProved, DONT ALLOW, return target's Capt/Amb & give a new card
-    // if blocked & playerChallenged, & !targetProved, ALLOW & take card from target
 
 
